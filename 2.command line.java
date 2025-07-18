55190312b0d136833acf74babf780c4ea7f520ad
Thereka <162158931+Thereka@users.noreply.github.com>
2025-07-18 20:48:02 +0530

    Create 2.command line.java

---
+++ 2.command line.java
@@ -0,0 +1,10 @@
+//Welcome message
+
+import java.util.*;
+class Main{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        System.out.print("Welcome "+str);
+    }
+}
