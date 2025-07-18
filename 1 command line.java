2f216a94fc2d5a49c21ae76cb043b14b57053959
Thereka <162158931+Thereka@users.noreply.github.com>
2025-07-18 20:47:27 +0530

    Create 1 command line.java

---
+++ 1 command line.java
@@ -0,0 +1,12 @@
+//Command Line string Get
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    String s1=sc.next();
+	    String s2=sc.next();
+	    String s3="Technologies";
+		System.out.println(s1+" "+s3+" "+s2);
+	}
+}
